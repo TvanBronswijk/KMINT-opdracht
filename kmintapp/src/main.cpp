8258d5148af3e6598553d030f3480cf4259cc55b
Tobi Bronswijk <294604@student.fontys.nl>
2018-11-05 16:30:03 +0100

    finished tutorial

--- kmintapp/src/main.cpp
+++ kmintapp/src/main.cpp
@@ -1,10 +1,57 @@
+#include <iostream>
 #include "kmint/main.hpp"          // voor de main loop
 #include "kmint/graphics.hpp"      // kleuren en afbeeldingen
 #include "kmint/math/vector2d.hpp" // voor window en app
 #include "kmint/play.hpp"          // voor stage
 #include "kmint/ui.hpp"            // voor window en app
+#include "kmint/map/map.hpp"
+#include "actors/cow.hpp"
+#include "actors/hare.hpp"
 
 using namespace kmint; // alles van libkmint bevindt zich in deze namespace
+
+static const char *map_description = R"graph(32 24 32
+resources/firstmap.png
+G 1 1
+C 1 1
+H 1 1
+W 0 0
+B 1 8
+
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWHGGGGGGGGGGGGGGGGGGGGGGGGHWWW
+WWWGGGGGGGGGGGGGGGGGGGGGGGGGGWWW
+WWWGGGGGGGGGGGGGBGGGGGGGGGGGGWWW
+WWWGGGGGGWWWWWWWBWWWWWWGGGGGGWWW
+WWWGGGGGGWWWWWWWBWWWWWWGGGGGGWWW
+WWWGGGGGGWWWWWWWBWWWWWWGGGGGGWWW
+WWWGGGGGGWWWWWGGBGWWWWWGGGGGGWWW
+WWWGGGGGGWWWWWGGGGWWWWWGGGGGGWWW
+WWWGGGGGGWWWWWGGGGWWWWWGGGGGGWWW
+WWWGGGGGBBBBBBBGCGGGGGGGGGGGGWWW
+WWWGGGGGGWWWWWGGGGWWWWWGGGGGGWWW
+WWWGGGGGGWWWWWGGBGWWWWWGGGGGGWWW
+WWWGGGGGGWWWWWWWBWWWWWWGGGGGGWWW
+WWWGGGGGGWWWWWWWBWWWWWWGGGGGGWWW
+WWWGGGGGGWWWWWWWBWWWWWWGGGGGGWWW
+WWWGGGGGGGGGGGGGBGGGGGGGGGGGGWWW
+WWWGGGGGGGGGGGGGGGGGGGGGGGGGGWWW
+WWWHGGGGGGGGGGGGGGGGGGGGGGGGHWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+)graph";
+
+const map::map_node &find_cow_node(const map::map_graph &graph) {
+	for (std::size_t i = 0; i < graph.num_nodes(); ++i) {
+		if (graph[i].node_info().kind == 'C') {
+			return graph[i];
+		}
+	}
+	throw "could not find starting point";
+}
 
 int main() {
   // een app object is nodig om
@@ -15,6 +62,19 @@ int main() {
 
   // maak een podium aan
   play::stage s{};
+
+  // Laad een kaart
+  map::map m{ map::read_map(map_description) };
+  s.build_actor<play::background>(
+	  math::size(1024, 768),
+	  graphics::image{ m.background_image() });
+  s.build_actor<play::map_actor>(
+	  math::vector2d{ 0.0f, 0.0f },
+	  m.graph());
+
+  auto &my_cow = s.build_actor<cow>(m.graph(), find_cow_node(m.graph()));
+  auto &my_hare = s.build_actor<hare>(m.graph());
+  my_hare.set_cow(my_cow);
 
   // Maak een event_source aan (hieruit kun je alle events halen, zoals
   // toetsaanslagen)
@@ -27,13 +87,13 @@ int main() {
     // loop controls is een object met eigenschappen die je kunt gebruiken om de
     // main-loop aan te sturen.
 
-    for (ui::events::event &e : event_source) {
-      // event heeft een methjode handle_quit die controleert
-      // of de gebruiker de applicatie wilt sluiten, en zo ja
-      // de meegegeven functie (of lambda) aanroept om met het
-      // bijbehorende quit_event
-      //
-      e.handle_quit([&ctl](ui::events::quit_event qe) { ctl.quit = true; });
-    }
+	  for (ui::events::event &e : event_source) {
+		  // event heeft een methode handle_quit die controleert
+		  // of de gebruiker de applicatie wilt sluiten, en zo ja
+		  // de meegegeven functie (of lambda) aanroept om met het
+		  // bijbehorende quit_event
+		  //
+		  e.handle_quit([&ctl](ui::events::quit_event qe) { ctl.quit = true; });
+	  }
   });
 }
